744c36edefc927b366df6bf41e86df28502d4cfd
Simon Thomas <simon.thomas1978@gmail.com>
2018-03-11 09:33:20 +0000

    game class setup

---
+++ src/kata-bowling/Game.cs
@@ -0,0 +1,16 @@
+﻿namespace kata.bowling
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+
+        }
+
+        public int Score()
+        {
+            return -1;
+        }
+
+    }
+}
