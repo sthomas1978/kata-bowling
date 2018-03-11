0fae5475274200b110c062648c3de4c514298c90
Simon Thomas <simon.thomas1978@gmail.com>
2018-03-11 10:07:15 +0000

    fix rolls a spare test

--- src/kata-bowling/Game.cs
+++ src/kata-bowling/Game.cs
@@ -14,7 +14,21 @@ namespace kata.bowling
 
         public int Score()
         {
-            return rolls.ToList().Sum();
+            int score = 0;
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (rolls[i] + rolls[i + 1] == 10)
+                {
+                    score = 10 + rolls[i + 2];
+                } else {
+                    score += rolls[i] + rolls[i + 1];
+                }
+
+                i += 2;
+            }
+
+            return score;
         }
 
     }
